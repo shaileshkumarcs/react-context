a0138b8d138d2d0abf70d633078b5bd14178330d
AzureAD\ShaileshKumarDangi <shailesh@bni.com>
2023-06-27 17:52:48 +0530

    Use reducer and Use effect use completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -42,34 +42,37 @@ const Login = (props) => {
     };
   },[]);
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(()=> {
-  //     console.log("Checking for validity");
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const identifier = setTimeout(()=> {
+      console.log("Checking for validity");
+      setFormIsValid(
+        emailIsValid && passwordIsValid
+      );
+    }, 500);
     
-  //   return () => {
-  //     console.log("CLEANUP");
-  //     clearTimeout(identifier);
-  //   };
-  // },[enteredEmail, enteredPassword]);
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
+  },[emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispachEmail({type: 'USER_INPUT', val: event.target.value})
 
-    setFormIsValid(
-      event.target.value.includes('@') && passwordState.isValid
-    );
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispachPassword({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
+    // setFormIsValid(
+    //   emailState.isValid && event.target.value.trim().length > 6
+    // );
   };
 
   const validateEmailHandler = () => {
